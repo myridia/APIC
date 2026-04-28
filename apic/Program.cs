e6d5d180d70aea03fabe59c217fbcb6f3fe10d20
veto <veto@body.body-piercing.com>
2026-04-28 10:56:05 +0700

    up: .gitignore LICENSE apic.slnx apic/.vscode/launch.json apic/.vscode/tasks.json apic/Program.cs apic/apic.csproj apic/libs/Interop.QBXMLRP2.dll apic/libs/Interop.QBXMLRP2Lib.dll apic/libs/QBXMLRP2.dll ask.sh build.sh

---
+++ apic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using System.ServiceProcess;
+using System.IO;
+using Tommy;
+
+namespace apic
+{
+  internal static class Program
+  {
+
+    public static void Main()
+    {
+
+      Console.WriteLine("Apic Gateway");
+    }
+  }
+}
+
+
---
+++ ask.sh
@@ -0,0 +1,55 @@
+echo -e "What you like to do?, enter a Task Id from list below: \n"
+echo -e "TaskID\tFile\t\tDescription"
+echo -e "1\t Build "
+echo -e "2\t Run "
+echo -e "3\t "
+echo -e "4\t "
+echo -e "5\t "
+echo -e "6\t "
+echo -e "7\t "
+echo -e "8\t "
+echo -e "9\t "
+
+until [ "$task" = "0" ]; do
+  read task
+
+  if [ "$task" = "1" ]; then
+    echo "...${task}"
+    dotnet publish apic/apic.csproj -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+
+  elif [ "$task" = "2" ]; then
+    echo "...${task}"
+    dotnet publish apic/apic.csproj -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+    apic/bin/Release/net10.0/linux-x64/apic
+
+  elif [ "$task" = "3" ]; then
+    echo "...${task}"
+    #scp  log/bin/Release/net10.0/linux-x64//publish/log  root@192.168.80.241:/opt/
+    #scp  stock/bin/Release/net10.0/linux-x64//publish/stock  root@192.168.80.241:/opt/
+  elif [ "$task" = "4" ]; then
+    echo "...${task}"
+
+  elif [ "$task" = "5" ]; then
+    echo "...${task}"
+
+  elif [ "$task" = "6" ]; then
+    echo "...${task}"
+
+  elif [ "$task" = "7" ]; then
+    echo "...${task}"
+
+  elif [ "$task" = "8" ]; then
+    echo "...${task}"
+
+  elif [ "$task" = "9" ]; then
+    echo "...${task}"
+
+  else
+    echo "Goodbye! - Exit"
+  fi
+
+  sleep 3
+
+  ./ask.sh
+
+done
