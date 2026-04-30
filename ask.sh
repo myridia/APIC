3c790a61a9ae5500dc43e196eb756a2db22e7aca
veto <veto@body.body-piercing.com>
2026-04-30 15:35:53 +0700

    up: .github/workflows/windows.yml README.md apic/Program.cs apic/apic.csproj ask.sh

--- ask.sh
+++ ask.sh
@@ -2,8 +2,8 @@ echo -e "What you like to do?, enter a Task Id from list below: \n"
 echo -e "TaskID\tFile\t\tDescription"
 echo -e "1\t Build "
 echo -e "2\t Run "
-echo -e "3\t "
-echo -e "4\t "
+echo -e "3\t Restores dependencies and tools of a project"
+echo -e "4\t List All Packages/Libraries with versions "
 echo -e "5\t "
 echo -e "6\t "
 echo -e "7\t "
@@ -24,10 +24,12 @@ until [ "$task" = "0" ]; do
 
   elif [ "$task" = "3" ]; then
     echo "...${task}"
+    dotnet restore
     #scp  log/bin/Release/net10.0/linux-x64//publish/log  root@192.168.80.241:/opt/
     #scp  stock/bin/Release/net10.0/linux-x64//publish/stock  root@192.168.80.241:/opt/
   elif [ "$task" = "4" ]; then
     echo "...${task}"
+    dotnet list package
 
   elif [ "$task" = "5" ]; then
     echo "...${task}"
